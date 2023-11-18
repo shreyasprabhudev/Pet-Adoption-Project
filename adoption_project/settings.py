9d63f0c0326362465790fea5caa2650bd0773a44
shreyasprabhudev <shreyasprabhudev@gmail.com>
2023-11-17 18:58:25 -0800

    finishing up update and del records

--- adoption_project/settings.py
+++ adoption_project/settings.py
@@ -59,9 +59,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'adoption_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
